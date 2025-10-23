83c095420c3758b4ecb33d6c4f3836e0ea31780e
daniel mendez <danielmenc@outlook.com>
2025-10-23 01:31:25 -0600

    #commit create new file to manage products

---
+++ src/features/inventory/services/mockData.ts
@@ -0,0 +1,7 @@
+import { Product } from "../types/Product";
+
+export const mockProducts: Product[] = [
+  { id: 1, name: 'Limpiador multiusos', quantity: 20, price: 45 },
+  { id: 2, name: 'Desinfectante en aerosol', quantity: 15, price: 60 },
+  { id: 3, name: 'Guantes de látex', quantity: 100, price: 5 },
+]
