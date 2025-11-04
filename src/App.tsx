b7efa36b3e052ff600c74c3369a7e2031bde08b4
daniel mendez <danielmenc@outlook.com>
2025-11-03 23:42:26 -0600

    #commit put on a new structure of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { DashboardPage } from "@/features/inventory/pages/DashboardPage";
-import { LoginPage } from "@/features/auth/pages/LoginPage";
-import PrivateRoute from "./app/routes/PrivateRoute";
+import { BrowserRouter, useNavigate } from "react-router-dom";
+import { AppRoutes } from "./routes/AppRoutes";
+import { NavigatorInitializer } from "./utils/NavigatorInitializer";
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { setNavigator } from "@/utils/NavigationService";
+import { setNavigator } from "./utils/NavigationService";
 
 function App() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    setNavigator(navigate);
-  }, [navigate]);
+  const NavigationInitializer = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+      setNavigator(navigate);
+    }, [navigate]);
+    return null;
+  }
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <DashboardPage />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <NavigatorInitializer />
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
