40a679965f9bde2d3bcc783bb33bb8455b4e9e1c
daniel mendez <danielmenc@outlook.com>
2025-11-07 00:13:11 -0600

    #commit ...

--- src/features/inventory/pages/DashboardPage.tsx
+++ src/features/inventory/pages/DashboardPage.tsx
@@ -8,12 +8,12 @@ export const DashboardPage = () => {
   const [products, setProducts] = useState<Product[]>(mockProducts)
 
   return (
-    <DashboardLayout>
+    <>
       <h1 className="mb-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Inventario</h1>
       <button className="mb-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
         Agregar Producto
       </button>
       <ProductTable products={products} />
-    </DashboardLayout>
+    </>
   );
 }
