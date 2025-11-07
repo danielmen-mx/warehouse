63226f2a6e9cdc5d9b72d4885a46a17fedcd553b
daniel mendez <danielmenc@outlook.com>
2025-11-07 00:13:33 -0600

    #commit create settingsPage

---
+++ src/features/settings/pages/SettingsPage.tsx
@@ -0,0 +1,12 @@
+import DashboardLayout from "@/app/layouts/DashboardLayout"
+
+export const SettingsPage = () => {
+  return (
+    <>
+      <div className="p-4">
+        <h1 className="text-xl font-bold">Configuraciones</h1>
+        <p>Aqui puedes ajustar tus preferencias, usuario, etc.</p>
+      </div>
+    </>
+  );
+}
